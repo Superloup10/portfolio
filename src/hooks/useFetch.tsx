abf90bdaa374a1c8a6e1530b90bd474a14e4fb52
Superloup10 <Superloup10@gmail.com>
2024-03-18 14:55:20 +0100

    Enhance useFetch hook with error handling and HTTP methods
    Introduce error state and expand the useFetch hook to support different HTTP methods and payloads. These changes enable error capturing and allow making POST, PUT, and DELETE requests in addition to GET.

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,21 +1,33 @@
 import {useEffect, useState} from "react";
 
 type FetchHook<T> = {
-    data: T[] | null
+    data: T[] | null;
+    error: any;
 };
 
-export default function useFetch<T>(url: string): FetchHook<T> {
+export default function useFetch<T>(url: string, method: "GET" | "POST" | "PUT" | "DELETE" = "GET", body?: any): FetchHook<T> {
     const [data, setData] = useState<T[] | null>(null);
+    const [error, setError] = useState<any>(null);
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch(url, {cache: "force-cache"});
-            if (response.ok) {
-                const data: T[] = await response.json();
-                setData(data);
+            const options: any = {method, cache: "force-cache", headers: {"Content-Type": "application/json"}};
+            if (body && (method === "POST" || method === "PUT")) {
+                options.body = JSON.stringify(body);
+            }
+            try {
+                const response = await fetch(url, options);
+                if (response.ok) {
+                    const data: T[] = await response.json();
+                    setData(data);
+                } else {
+                    setError(Error("Server error occurred"));
+                }
+            } catch (error) {
+                setError(error);
             }
         };
         fetchData();
-    }, [url]);
+    }, [url, method, body]);
 
-    return {data};
+    return {data, error};
 }
