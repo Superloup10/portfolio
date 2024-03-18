c54c01f8e319c761847e3250c20ef0b04f5c9222
Superloup10 <Superloup10@gmail.com>
2024-03-18 10:50:13 +0100

    Refactor useFetch to remove fetchAgain method
    Moved the fetchData function inside the useEffect hook to simplify the hook's API and removed the fetchAgain method that was previously exposed. The hook now only returns the fetched data.

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,23 +1,21 @@
 import {useEffect, useState} from "react";
 
 type FetchHook<T> = {
-    data: T[] | null,
-    fetchAgain: () => Promise<void>
+    data: T[] | null
 };
 
 export default function useFetch<T>(url: string): FetchHook<T> {
     const [data, setData] = useState<T[] | null>(null);
-    const fetchData = async () => {
-        const response = await fetch(url, {cache: "force-cache"});
-        if (response.ok) {
-            const data: T[] = await response.json();
-            setData(data);
-        }
-    };
-
     useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url, {cache: "force-cache"});
+            if (response.ok) {
+                const data: T[] = await response.json();
+                setData(data);
+            }
+        };
         fetchData();
     }, []);
 
-    return {data, fetchAgain: fetchData};
+    return {data};
 }
